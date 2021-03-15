65d92e9a625f782e94e599af38395969629fcd02
Fritz-Stevenson <fritzstevenson@gmail.com>
2021-03-14 23:21:47 -0700

    Added readability to the scripts, refined and streamlined the code, and corrected/implemented the datetime functionality.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 date_1 ='2020-12-31 23:52'
 date_2 = datetime.now()
 date_2 = date_2.strftime("%Y-%m-%d %H:%M")
@@ -6,7 +7,6 @@ date_3 = '2020-06-12 23:52'
 date_4 = '2020-03-05 23:52'
 date_list = [date_1, date_2, date_3, date_4]
 date_list.sort()
-print(date_list)
 date_1 = datetime.strptime(date_1, "%Y-%m-%d %H:%M")
 date_2 = datetime.strptime(date_2, "%Y-%m-%d %H:%M")
 date_3 = datetime.strptime(date_3, "%Y-%m-%d %H:%M")
@@ -14,4 +14,6 @@ date_4 = datetime.strptime(date_4, "%Y-%m-%d %H:%M")
 date_list = [date_1, date_2, date_3, date_4]
 
 date_list.sort()
-print(date_list)
+num_list = [60,50,40]
+f= ', '.join(map(str, num_list))
+print(f)
--- utility.py
+++ utility.py
@@ -1,29 +1,39 @@
 import pandas as pd
 import os
 import customer_profile as cpm
+from datetime import datetime
 def clean(inp):
+    ''' Cleans a dataframe used for analysis, removing erroneous characters from columns.'''
     obj = inp
     obj.columns = obj.columns.str.replace(' ','_')
     obj.columns = obj.columns.str.replace('(', '')
     obj.columns = obj.columns.str.replace(')', '')
     obj.fillna(0)
     return obj
-# make a concatenate functions
+
 def concat(dir_name):
-    frame = pd.DataFrame()
+    '''Concatenates all CSVs in the CSV_Files folder.'''
     abs_file_path = os.path.abspath(dir_name)
+    os.chdir(abs_file_path)
+    frame_list =[]
     for file in os.listdir(abs_file_path):
         if file.endswith('.csv'):
-            frame.concat(pd.read_csv(file))
+            file = os.path.abspath(file)
+            sub_frame = pd.read_csv(file)
+            frame_list.append(sub_frame)
+    frame = pd.concat(frame_list, ignore_index=True)
+    os.chdir('C:\\Users\\fritz\\PycharmProjects\\Brothers_Analysis')
     return frame
             
 class FieldLoader:
+    '''Class that reads the dataframe and serves customer_profile.SalesRow the appropriate set of parameters.'''
     def __init__(self, frame):
         self.frame = frame
         self.row_number = 0
         self.gen_wrapper()
 
     def gen_wrapper(self):
+        '''Serves each row as an individual SalesRow() call'''
         for i in range(len(self.frame.index)):
             self.row_number = i
             final_list = self.gen_fields()
@@ -31,6 +41,7 @@ class FieldLoader:
             init_SalesRow
 
     def gen_fields(self):
+        '''Sets appropriate cells as parameters, in the correct order '''
         #how about making this a list of lists, and making each row accessible as the fields
         # of a customer_profile_module.SalesRow call
         name = self.frame.loc[:, 'Full_Name_Shipping']
@@ -48,39 +59,83 @@ class FieldLoader:
             raw_product_quantity_cell, raw_item_cost_cell, total_gross_sales)
         return list(field_zip)[self.row_number]
 
-def date_difference_compiler():
+def date_difference_email_list_compiler():
+    '''Calculates the difference in days between purchases, including the present time as a 'purchase'
+    in order to calculate the time since last purchase.'''
+    #You must run customer_profile.dates_differential() before this function or it will not operate correctly
     retail = os.listdir(os.getcwd()+'\\retail_customers')
     wholesale = os.listdir(os.getcwd()+'\\wholesale_customers')
     full_set =[]
     day_threshold = 40 # This number will control the days since previous sale threshold for email campaigns
-    for i in retail, wholesale:
-        c = pd.read_csv('.\\retail_customers\\'+i)
+    for i in retail:
+        c = pd.read_csv('.\\retail_customers\\'+ i)
+        #following are fields that can be formatted for output
         name = c.at[0,'Value']
         email = c.at[1, 'Value']
-        dd_count = c.at[5, 'Totals']
+        dd_count = c.iat[7, 1]
         dd_count =dd_count.split('\n')
-        print(dd_count)
-        print(type(dd_count))
-        if type(dd_count)!= list:
-            if int(dd_count) > day_threshold:
-                full_set.append(email)
         if type(dd_count) == list:
             if int(dd_count[-1])> day_threshold:
-                full_set.append(email)
-        print(dd_count)
-    for i in wholesale:
-        c = pd.read_csv('.\\wholesale_customers\\' + i)
+                full_set.append(email) #output list addition
+    for x in wholesale:
+        c = pd.read_csv('.\\wholesale_customers\\' + x)
         name = c.at[0, 'Value']
         email = c.at[1, 'Value']
-        dd_count = c.at[5, 'Totals']
+        dd_count = c.iat[7,1]
         dd_count.split('\n')
-        print(dd_count)
-        print(type(dd_count))
         if type(dd_count) != list:
             if int(dd_count) > day_threshold:
                 full_set.append(email)
         if type(dd_count) == list:
             if int(dd_count[-1]) > day_threshold:
                 full_set.append(email)
-        print(dd_count)
     return print(full_set)
+def date_difference():
+    change_count=0
+    retail = os.listdir(os.getcwd() + '\\retail_customers')
+    wholesale = os.listdir(os.getcwd() + '\\wholesale_customers')
+    for i in retail:
+        difference_list = []
+        c = pd.read_csv('.\\retail_customers\\'+ i)
+        dates = c.iat[6,1]
+        dates =dates.split('\n')
+        dates.remove('')
+        present = datetime.now()
+        current_stripped = present.strftime("%Y-%m-%d %H:%M")
+        dates.append(current_stripped)
+        dates = sorted(list(dates))
+
+        for x in range(len(list(dates))):
+            if x == len(list(dates))-1:
+                break
+            d1 = dates[x]
+            d2 = dates[x + 1]
+            d1 = datetime.strptime(d1, "%Y-%m-%d %H:%M")
+            d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M")
+            difference = d2 - d1
+            difference_list.append(difference.days)
+        c.iat[7,1] = '\n'.join(map(str, difference_list))
+        c.to_csv('.\\retail_customers\\'+ i, index=False)
+        change_count +=1
+    for i in wholesale:
+        difference_list = []
+        c = pd.read_csv('.\\wholesale_customers\\' + i)
+        dates = c.iat[6, 1]
+        dates = dates.split('\n')
+        dates.remove('')
+        present = datetime.now()
+        current_stripped = present.strftime("%Y-%m-%d %H:%M")
+        dates += current_stripped
+        dates = sorted(list(dates))
+        for i in range(len(list(dates))):
+            if i == len(list(dates))-1:
+                break
+            d1 = dates[i]
+            d2 = dates[i + 1]
+            d1 = datetime.strptime(d1, "%Y-%m-%d %H:%M")
+            d2 = datetime.strptime(d2, "%Y-%m-%d %H:%M")
+            difference = d2 - d1
+            difference_list.append(difference.days)
+        c.iat[7, 1] = '\n'.join(map(str, difference_list))
+        c.to_csv('.\\retail_customers\\' + i, index=False)
+        change_count +=1
