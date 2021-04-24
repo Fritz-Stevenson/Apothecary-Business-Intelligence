d52662be90a2ce02708d95b22b79c16d02a07d8e
Fritz-Stevenson <fritzstevenson@gmail.com>
2021-04-23 23:20:52 -0700

    Beginning of new module, which will compile unit based sales by product and calculate the total volume of ingredients from a Product Sales Report CSV. Need to connect totals to list of dictionaries for ingredients.

--- information_repository.py
+++ information_repository.py
@@ -1,13 +1,13 @@
-tea_product_list = ['Breakfast Buzz', 'Buddha\'s Berry', 'Chai Awakening', 'Coconut Genmaicha', 'Cosmic Cleanse',
+tea_product_list = ['Breakfast Buzz', 'Buddha', 'Chai Awakening', 'Coconut Genmaicha', 'Cosmic Cleanse',
                     'Golden Dream', 'Highbiscus', 'Hemp-Derived CBD Tea', 'Mellow Mint', 'Mystic', 'Oolong Passion',
                     'Pumpkin Spice', 'Sensualitea', 'Tea Gift 8-Pack']
 capsule_product_list = ['Blood Flow', 'Cleanse | CBD', 'Digest Well', 'Immunity Support', 'Mental Clarity', 'Sleep Aid',
-                        'Super Citrus', 'Super Greens', 'Supreme Vitality', ]
+                        'Super Citrus', 'Super Greens', 'Supreme Vitality' ]
 smokeable_product_list = ['CBG Flower', 'Center', 'Kush Hemp CBD Flower', 'Peace', 'Relax', 'Sacred']
 skincare_product_list = ['Buzz Balm', 'Focus', 'Insight', 'Love', 'Pain Away', 'Restful', 'Slumbering CBD Bath Soak',
                          'Soothing CBD Body Butter', 'Renewing CBD Face Cream', 'Unwind', 'Sugar Scrub']
 superfood_product_list = ['Blue Moon', 'Chocodelic Trip', 'Garden of Eden', 'Golden Milk', 'Mighty Matcha',
-                          'Peppermint Treat', ]
+                          'Peppermint Treat','Magickal Mushroom']
 honey_product_list = ['Hibiscus Goji Berry', 'Lavender', 'Lemon Ginger',
                       'Orange Turmeric', 'Organic Wildflower', 'Wild Rosin Honey' ]
 tincture_product_list = ['Dragon\'s Lair', 'Stomach Soother', 'Calming K9']
